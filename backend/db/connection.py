a8bb618fb40719a0f5534d03db5a1651a0854648
Ritesh Singh <iamriteshsingh03@gmail.com>
2026-09-07 23:21:05 +0530

    Deployment Fix

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -11,5 +11,5 @@ DB_HOST = os.environ["DB_HOST"]
 DB_PORT = os.environ["DB_PORT"]
 DB_NAME = os.environ["DB_NAME"]
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
 engine = create_engine(DATABASE_URL)
